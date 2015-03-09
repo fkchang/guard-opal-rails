77139701b25d587ef7f66c193ca9b5adf0a63701
Forrest Chang <fchang@hedgeye.com>
2015-03-09 13:12:13 -0700

    add spec rake task, update to expect syntax

--- spec/lib/guard/opal-rails/standalone_spec_server_process_toggle_spec.rb
+++ spec/lib/guard/opal-rails/standalone_spec_server_process_toggle_spec.rb
@@ -2,12 +2,12 @@ require 'guard/opal-rails/standalone_spec_server_process_toggle'
 describe Guard::OpalRails::StandaloneSpecServerProcessToggle do
   it 'should call the right rake task to start the server' do
     # subject.should_receive(:run_task).with 'opal:spec:start_server'
-    subject.should_receive(:system).with 'bundle exec rake opal:spec:start_server'
+    expect(subject).to receive(:system).with 'bundle exec rake opal:spec:start_server'
     subject.start
   end
   it 'should call the right rake task to stop the server' do
     # subject.should_receive(:run_task).with 'opal:spec:kill_server'
-    subject.should_receive(:system).with 'bundle exec rake opal:spec:kill_server'
+    expect(subject).to receive(:system).with 'bundle exec rake opal:spec:kill_server'
     subject.stop
   end
 end
